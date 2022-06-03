d9d806d2525363c89b12f345ea460896d90cb73a
malik <akinwalemalik64@gmail.com>
2022-06-03 03:55:42 +0000

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ "programming is like building a multilingual puzzle
-	    return (0);
-}
+{ 
+puts("programming is like building a multilingual puzzle);
+return (0);
+}:wq
+
